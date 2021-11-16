0d84c156e8cb2bfc6a5376436a51568538e5b1ed
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-11-16 14:41:31 -0600

    Added the target blank

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ export default function App() {
       <h1>React weather pro</h1>
       <footer>
          This project was coded by Maria Garcia {""}
-         <a href="https://github.com/mariasalud/react-weather-pro" target="_blank">
+         <a href="https://github.com/mariasalud/react-weather-pro" target="_blank" rel="noreferrer">
         Open-source on Github</a> and on {""}
-       <a href="https://hardcore-hugle-da8414.netlify.app">Netlify</a> 
+       <a href="https://hardcore-hugle-da8414.netlify.app" target="_blank" rel="noreferrer">Netlify</a> 
       </footer>
       </div>
-      );
+  );
 }
 
 
