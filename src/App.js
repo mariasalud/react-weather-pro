6ef8c63d6857fc58ba0a0cae9cef91e56ead7bf3
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-11-16 17:04:53 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>React weather pro</h1>
+      <div className="container">
+      <h1>React weather project</h1>
       <footer>
          This project was coded by Maria Garcia {""}
          <a href="https://github.com/mariasalud/react-weather-pro" target="_blank" rel="noreferrer">
         Open-source on Github</a> and on {""}
        <a href="https://hardcore-hugle-da8414.netlify.app" target="_blank" rel="noreferrer">Netlify</a> 
       </footer>
+      </div>
       </div>
   );
 }
