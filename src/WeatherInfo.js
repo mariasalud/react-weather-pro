bc5fa852c1da828efe7113f08b672d451e45553b
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-11-19 13:28:13 -0600

    Added a new component made the icon to work.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -11,22 +12,20 @@ export default function WeatherInfo(props) {
               </li>
             <li className="text-capitalize">{props.data.description} </li>
         </ul>
-           <div className="row mt-5">
+           <div className="row mt-3">
              <div className="col-6">
-                 <div className="clearfix">
-             <img 
-             src={props.data.iconUrl} 
-             alt={props.data.description}
-             width="100px" 
-             className="float-left"
-             />
+              <div className="clearfix">
+                <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+              </div>
+             
              <div className="float-left">
                <span className="temperature">{Math.round(props.data.temperature)}</span> 
                <span className="unit"> °C </span>
                </div>
                </div>
              </div>
-             <div className="col-6">
+             <div className="col-6" >
                <ul> 
                    <li>Precipitation: 4 %</li>
                     <li>Humidity: {props.data.humidity} MPH</li>
